omapconf/omapconf

commit 9c1fbecc33f11e0586ea0c41580bcb0547ee648b

[DPLL] [DRA7] Fix HSDIV clock rate
Rate of HSDIV is shown as fdpll/div even though it is disabled. This
may fail sometimes when divider is by default 0 when disabled. So, fix
it by making rate as fdpll when HSDIV is disabled.

Acked-by: Praneeth Bajjuri <praneeth@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
Signed-off-by: Nishanth Menon <nm@ti.com>

arch/arm/mach-omap/dra7/dpll/dpll_dra7xx.c
@@ -1007,8 +1007,11 @@ int hsdiv_dra7xx_rates_calc(dpll_dra7xx_settings *settings,
 					(settings->hsdiv)[i].rate = (settings->dpll).fdpll /
 						2.5 / 2.0;
 			} else {
-				(settings->hsdiv)[i].rate = (settings->dpll).fdpll /
-					(double) (settings->hsdiv)[i].div;
+				if (!(settings->hsdiv)[i].status)
+					(settings->hsdiv)[i].rate = (settings->dpll).fdpll;
+				else
+					(settings->hsdiv)[i].rate = (settings->dpll).fdpll /
+						(double) (settings->hsdiv)[i].div;
 			}
 
 			dprintf("%s(): %s locked, %s=%lfMHz\n", __func__,

commit 40ab0a2a57dffcf7e2813fc7a526d6cde3755347

[DPLL][DRA7] Fix calculation of hs div clk rate
commit 9c1fbecc33f11 ("[DPLL] [DRA7] Fix HSDIV clock rate") updates the
calculation of hs div rates based on the hs div output status.
Apparently there are many hs dividers which are programmed but the
corresponding clock outputs are gated. In order to give the similar
behaviour to kernel, use divider value to check if the divider is valid.

Fixes: 9c1fbecc33f11 ("[DPLL] [DRA7] Fix HSDIV clock rate")
Debugged-by: Suman Anna <s-anna@ti.com>
Acked-by: Suman Anna <s-anna@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

arch/arm/mach-omap/dra7/dpll/dpll_dra7xx.c
@@ -1030,7 +1030,7 @@ int hsdiv_dra7xx_rates_calc(dpll_dra7xx_settings * settings,
 					(settings->hsdiv)[i].rate =
 					    (settings->dpll).fdpll / 2.5 / 2.0;
 			} else {
-				if (!(settings->hsdiv)[i].status)
+				if (!(settings->hsdiv)[i].div)
 					(settings->hsdiv)[i].rate =
 					    (settings->dpll).fdpll;
 				else